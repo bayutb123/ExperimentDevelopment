41f5b4b3d828905a03c0324c1bfd60932aef8e7c
Bayu Tantra Bramandhita <73697375+bayutb123@users.noreply.github.com>
2023-10-11 18:07:06 +0700

    Remove unused import

--- app/src/main/java/com/bayutb123/experimentdevelopment/components/input/CustomTextField.kt
+++ app/src/main/java/com/bayutb123/experimentdevelopment/components/input/CustomTextField.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -30,7 +29,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.bayutb123.experimentdevelopment.ui.theme.ExperimentDevelopmentTheme
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CustomTextField(
     modifier: Modifier = Modifier,
